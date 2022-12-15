e3a8a045e62cef1f7f2e497d1fd3e33a8c823b6d
Tejas Pradeep Thigale <120454034+thigaletejas18@users.noreply.github.com>
2022-12-15 20:32:30 +0530

    Update AddServlet.java

--- src/main/java/AddServlet.java
+++ src/main/java/AddServlet.java
@@ -32,6 +32,13 @@ public class AddServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		PrintWriter pw=response.getWriter();
 		int rollno=Integer.parseInt(request.getParameter("rollno"));
 		String name=request.getParameter("name");
@@ -57,14 +64,6 @@ public class AddServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		pw.println("<a href='Index.html'>Back To Menu</a>");
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
